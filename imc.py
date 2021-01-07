e64102a8148fcb14742ebc9a4ca6f3d76ce0a5e5
hidekimusashi <61999819+hidekimusashi@users.noreply.github.com>
2021-01-07 17:24:26 -0300

    Add files via upload

---
+++ imc.py
@@ -0,0 +1,11 @@
+def numero_quadrado(numero):
+  quadrado = numero * numero
+  return quadrado
+
+def imc(peso, altura):
+  altura_quadrada = altura ** 2
+  meu_imc = peso / altura_quadrada
+  print(f'O meu imc é {meu_imc:.2f}') 
+  return meu_imc
+
+meu_imc = imc(66, 1.75)
